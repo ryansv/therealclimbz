e654f8b6eff7d63400ef60fc43a48fb783b2c5ec
Brigit Lyons <blyons@ubuntu.(none)>
2013-12-01 13:00:02 -0800

    brigit testing pull request

--- climbz/src/com/dab/climbz/MainActivity.java
+++ climbz/src/com/dab/climbz/MainActivity.java
@@ -2,6 +2,7 @@ package com.dab.climbz;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -17,6 +18,7 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+    	Log.d(LOG_TAG, "onCreateOptionsMenu entry");
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
