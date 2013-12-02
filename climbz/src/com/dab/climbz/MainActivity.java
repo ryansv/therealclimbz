311463f2e0ec10754f15934c144a0cbb97b5edb8
climbz <derek.locke@gmail.com>
2013-12-02 00:35:09 -0500

    made simple listView UI

--- climbz/src/com/dab/climbz/MainActivity.java
+++ climbz/src/com/dab/climbz/MainActivity.java
@@ -1,25 +1,50 @@
 package com.dab.climbz;
 
+import android.R.color;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ListActivity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
+import android.widget.ListView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
 	
 	private static final String LOG_TAG = "MainActivity";
+	private String[] mRouteNames= {"awesome 5.10","shitty 5.9","crimpy 12c","choss","awesome 5.10","shitty 5.9","crimpy 12c","choss",
+			"awesome 5.10","shitty 5.9","crimpy 12c","choss"};
+	
+	private ListView mRouteListView;
+	private ArrayAdapter mArrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //getlist
         setContentView(R.layout.activity_main);
+        
+        mRouteListView = getListView();
+        mArrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_multiple_choice, mRouteNames);
+        //setListAdapter(mArrayAdapter); //same as function below?
+        mRouteListView.setAdapter(mArrayAdapter);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        
+    	//work on determining whether text or checkbox is clicked...
+    	
+    	CheckedTextView check = (CheckedTextView)v;
+        check.setChecked(!check.isChecked());
     }
     
 }
